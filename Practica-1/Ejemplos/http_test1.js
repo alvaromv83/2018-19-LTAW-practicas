105a093709ee0f76d8fd450273f85f7c54731903
Alvaro Moles Vinader <amoles@delta27.aulas.gsyc.urjc.es>
2019-02-04 11:57:40 +0100

    Servidor de ejemplo hecho en clase

---
+++ Practica-1/Ejemplos/http_test1.js
@@ -0,0 +1,21 @@
+// Notación "no expandida" de cómo funciona un servidor
+
+var http = require('http');
+
+console.log("Arrancando servidor...")
+
+//-- Configurar el servidor. Cada vez que llegue una peteicion se emite un
+//-- evento y se invoa a la funcion server_req
+server = http.createServer(server_req) // Crea un servidor web, y cuando recibe cualquier petición
+                                       // invoca a la función de retrollamada
+
+//-- Funcion de retrollamada de servicio de las peticiones
+//-- No se devuelve mensaje, se indica en consola que ha llegado
+//-- una peticion
+function server_req(req, res) {   // La API me dice que la función siempre tiene 2 parámetros:
+                                  // solicitud y respuesta
+  console.log("---> Peticion recibida")
+}
+
+//-- Queremos que el servidor escuche peticiones en puerto 8080
+server.listen(8080);
---
+++ Practica-1/Ejemplos/http_test3.js
@@ -0,0 +1,12 @@
+// Segunda compactación -->   ESTA ES LA QUE VAMOS A USAR
+
+var http = require('http');
+
+console.log("Arrancando servidor...")
+
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
+http.createServer((req, res) => { // Sigue ejecutando por la línea 11 y espera peticiones.
+                                  // Cuando llega una petición ejecuta el cuerpo
+  console.log("---> Peticion recibida")
+}).listen(8080);
