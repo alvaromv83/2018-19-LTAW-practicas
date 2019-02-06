dee83ded7b98732673fa6bdcfd7631fbbe98b12b
Álvaro <alvaro@Alvaros-MacBook-Pro.local>
2019-02-06 15:17:28 +0100

    Gestión de errores

--- Practica-1/web2/server.js
+++ Practica-1/web2/server.js
@@ -1,15 +1,29 @@
 var http = require('http');    // Con esto cogemos la biblioteca
 var fs = require('fs');        // Método para leer ficheros en el ordenador
+var url = require('url');
 
-console.log("Arrancando servidor...")   // Traza
+console.log("Arrancando servidor...")
 
 // Función que invoca al servidor
-http.createServer(function (req, res) {                 // Crea un servidor. Cada vez que haya una petición,
-                                                        // llama a las funciones que le paso como parámetros
-  fs.readFile('main.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-    console.log("Peticion atendida")   // Traza
+http.createServer((req, res) => {
+
+  var url_obj = url.parse(req.url, true)
+  var path = 'static' + url_obj.pathname
+
+  console.log("Recurso solicitado (URL): " + path)
+
+  fs.readFile('static/index.html', function(err, data) {
+    if (!err) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+      console.log("Peticion atendida")
+    } else {
+      res.writeHead(404, {'Content-Type':'text/html'});
+      res.write('Error 404 - Recurso inexistente')
+      res.end();
+      console.log("Recurso no encontrado")
+    }
+
   });
 }).listen(8080);
