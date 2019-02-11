744c7f0b7a5c7890d9e2a314794cd49eeccb6c54
Alvaro Moles Vinader <amoles@beta29.aulas.gsyc.urjc.es>
2019-02-11 12:23:36 +0100

    Funciona CSS

--- Practica-1/server.js
+++ Practica-1/server.js
@@ -1,32 +1,63 @@
-var http = require('http');    // Con esto cogemos la biblioteca
-var fs = require('fs');        // Método para leer ficheros en el ordenador
+var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
-console.log("Arrancando servidor...")
+console.log("Arrancando servidor...\n")
 
-// Función que invoca al servidor
+//-- Configurar y lanzar el servidor. Por cada peticion recibida
+//-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
+  console.log("Recurso solicitado (URL): " + req.url)
+  var q = url.parse(req.url, true)
+//  var q = 'static' + q.pathname
+  console.log("URL parseada: ")
+  console.log("   Host: " + q.host)
+  console.log("   Path Name:" + q.pathname)
 
-  var url_obj = url.parse(req.url, true)
-  var path = 'static' + url_obj.pathname
+  //-- Obtener el fichero. Si es "static/" se toma index.html
+  //-- Poner el "." delante para que sean un fichero del directorio actual
 
-  if (path == 'static/')
-    path = 'static/index.html';
+  var filename = ""
 
-  console.log("Recurso solicitado: " + path)
+  if (q.pathname == "/")
+    filename += "/index.html"
+  else {
+    filename = q.pathname
+  }
 
-  fs.readFile(path, function(err, data) {
-    if (!err) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-      console.log("Petición atendida")
-    } else {
-      res.writeHead(404, {'Content-Type':'text/html'});
-      res.write('Error 404 - Recurso inexistente')
-      res.end();
-      console.log("Recurso no encontrado")
+  //-- Obtener el tipo de fichero segun la extension
+  filetype = filename.split(".")[1]
+
+  //-- Obtener el nombre del fichero a partir del recurso solicitado
+  //-- Se añade un . delante
+  filename = "." + filename
+
+  console.log("Ruta del fichero: " + filename)
+  console.log("Tipo de fichero: " + filetype + "\n")
+
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
     }
 
+    //-- Tipo mime por defecto: html
+    var mime = "text/html"
+
+    //-- Es un css
+    if (filetype == "css")
+      mime = "text/css"
+
+    //-- Es una imagen
+    if (['png', 'jpg'].includes(filetype)) {
+      console.log("IMAGEN!!!!!")
+      mime = "image/" + filetype
+    }
+
+    //-- Generar el mensaje de respuesta
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
   });
+
 }).listen(8080);
