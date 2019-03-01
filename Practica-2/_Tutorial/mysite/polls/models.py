0e79bb54239d650ea7636a54ca9d8602ea63b0ea
Álvaro <alvaro@Alvaros-MacBook-Pro.local>
2019-03-01 10:53:19 +0100

    Tutorial Django

---
+++ Practica-2/_Tutorial/mysite/polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# ------- Modelos ----------
+# (cada variable del modelo representa un campo de la BD en el modelo)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
