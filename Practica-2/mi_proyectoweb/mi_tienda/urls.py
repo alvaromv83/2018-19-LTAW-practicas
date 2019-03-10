1843fc4e254c08cae19e1ebe5a1b4fde2ff1b54e
Álvaro <alvaro@Alvaros-MacBook-Pro.local>
2019-03-10 13:34:32 +0100

    P2: update

--- Practica-2/mi_proyectoweb/mi_tienda/urls.py
+++ Practica-2/mi_proyectoweb/mi_tienda/urls.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
-from . import views
+from . import views     # Con esto le decimos: coge todas las vistas creadas en
+                        # mi_tienda e impórtalas.
+from mi_tienda.views import cuerda_view
 
 urlpatterns = [
     url(r'^$', views.home_view),
-    url(r'^$', views.cuerda_view),
+    url(r'^cuerda/', views.cuerda_view),
+    url(r'^percusion/', views.percusion_view),
+    url(r'^viento/', views.viento_view),
+    url(r'^list/', views.list),
 ]
--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -4,9 +4,27 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from mi_tienda.models import Product
+
 # Create your views here.
 def home_view (request):
     return render(request, "index.html", {})
 
 def cuerda_view (request):
     return render(request, "cuerda.html", {})
+
+def percusion_view (request):
+    return render(request, "percusion.html", {})
+
+def viento_view (request):
+    return render(request, "viento.html", {})
+
+def list(request):
+    objects = Product.objects.all() # Creamos una lista que lee de la BD todos
+                                    # los elementos
+    html = "<p>Listado de articulos</p>"
+    for elt in objects:
+        print(elt.name)     # Debug (consola)
+        html += '<p>'+ elt.name + ' ' + str(elt.price) + '<p>'  # Añadimos al
+                                                                # HTML
+    return HttpResponse(html)   # Enviamos la respuesta
