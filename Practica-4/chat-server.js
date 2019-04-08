bf650d6fca9cf50a20886ddcf9dbb73e91f8a65b
Álvaro <alvaro@Alvaros-MacBook-Pro.local>
2019-04-08 11:00:38 +0200

    P4: IDs de usuarios

--- Practica-4/chat-server.js
+++ Practica-4/chat-server.js
@@ -30,23 +30,26 @@ http.listen(PORT, function(){
 io.on('connection', function(socket){
   console.log('--> Usuario conectado');
 
-  // Enviar mensaje de bienvenida al nuevo usuario
-  socket.emit('new_message', 'Bienvenido'); //socket.emit es unicast
-
-  // Anuncio del nuevo usuario
-  io.emit('new_message', 'Nuevo usuario conectado'); //io.emit es broadcast
-
   n_users += 1;
+  var random_number =  Math.floor((Math.random() * 100000) + 1);
+  socket.id = "User_" + random_number
+
+  console.log("ID del usuario: " + socket.id)
   console.log("Número de usuarios en el chat: " + n_users);
 
-  // Anunciar el nuevo usuario al resto de usuarios
+
+  // Enviar mensaje de bienvenida al nuevo usuario
+  socket.emit('new_message', '<i>Bienvenido al chat ' + socket.id + "</i>"); //socket.emit es unicast
+
+  // Anuncio del nuevo usuario
+  io.emit('new_message', "<i>" + socket.id + ' se ha conectado al chat</i>'); //io.emit es broadcast
 
   //-- Detectar si el usuario se ha desconectado
   socket.on('disconnect', function(){
     console.log('--> Usuario Desconectado');
 
     // Enviar mensaje de bienvenida al nuevo usuario
-    io.emit('new_message', 'Usuario desconectado');
+    io.emit('new_message', "<i>" + socket.id + ' ha salido del chat</i>'); //io.emit es broadcast
 
     n_users -= 1;
     console.log("Número de usuarios en el chat: " + n_users);
@@ -63,31 +66,32 @@ io.on('connection', function(socket){
     if (client_msg.startsWith("/")) {
       switch(client_msg) {
         case "/help":
-          server_msg = "Lista de comandos: /help, /list, /hello, /date"
+          server_msg = "<i>Lista de comandos: /help, /list, /hello, /date</i>"
           socket.emit('new_message', server_msg)
           console.log("Respuesta a /help")
         break;
         case "/list":
-          server_msg = "Número de usuarios conectados: " + n_users
+          server_msg = "<i>Número de usuarios conectados: </i>" + n_users
           socket.emit('new_message', server_msg)
           console.log("Respuesta a /list")
         break;
         case "/hello":
-          socket.emit('new_message', "Hola")
+          socket.emit('new_message', "<i>Hola</i>")
           console.log("Respuesta a /hello")
         break;
         case "/date":
           var date = new Date()
-          server_msg = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+          server_msg = "<i>" + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</i>"
           socket.emit('new_message', server_msg)
           console.log("Respuesta a /date")
         break;
         default:
-          socket.emit('new_message', 'Comando no encontrado')
+          socket.emit('new_message', '<i>Comando no encontrado</i>')
           console.log("Comando no encontrado")
       }
     } else {
       //-- Emitir un mensaje a todos los clientes conectados
+      client_msg = "<b>" + socket.id + "</b>: " + client_msg
       io.emit('new_message', client_msg);   // Broadcast
     }
   })
