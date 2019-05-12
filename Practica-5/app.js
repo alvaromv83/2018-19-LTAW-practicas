3104f1b9f0d642be242e9b863c29c2de27a27f73
Álvaro <alvaro@Alvaros-MacBook-Pro.local>
2019-05-12 10:22:17 +0200

    Chat Electron terminado

--- Practica-5/app.js
+++ Practica-5/app.js
@@ -8,14 +8,35 @@ function main()
   //-- Obtener los elementos del interfaz, del DOM
   let button = document.getElementById('button')
   let display = document.getElementById('display')
+  var msg = document.getElementById("msg")
 
-  //-- Cuando se aprieta el botón...
+  //-- Caja con el mensaje a enviar
+  var msg = document.getElementById("msg")
+
+  msg.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("send").click()
+    }
+  });
+
+  //-- Cuando se aprieta el botón de enviar...
   button.onclick = () => {
 
-    //-- Sacar un mensaje en la consola
-    console.log("click!")
+    //-- Enviar el mensaje, con el evento "new_message"
+    socket.emit('new_message', msg.value);
 
-    //-- Añadir la cadena al párrafo
-    display.innerHTML += "holi "
+    //-- Limpiar la Caja
+    document.getElementById('msg').value = ""
+
+    //-- Lo notificamos en la consola del navegador
+    console.log("--> Mensaje emitido: " + msg.value)
   }
+
+  //-- Cuando se reciba un mensaje del servidor se muestra
+  //-- en el párrafo
+  socket.on('new_message', msg => {
+    display.innerHTML = display.innerHTML + "<br>" + "<nobr>>>> </nobr>" + msg;
+    console.log("--> Mensaje recibido: " + msg)
+  });
 }
